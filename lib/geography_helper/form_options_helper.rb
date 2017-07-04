4bfe8bbc80844d35ae317cd37dbd6bd0c5106c04
Eric Iacutone <eric.iacutone@gmail.com>
2017-07-03 21:11:04 -0500

    Add selected state value

--- lib/geography_helper/form_options_helper.rb
+++ lib/geography_helper/form_options_helper.rb
@@ -2,13 +2,13 @@ module ActionView
   module Helpers
     module FormOptionsHelper
 
-      def state_options_for_select
+      def state_options_for_select(selected=nil)
         geography = GeographyHelper::States.new
         state_options = geography.states
         province_options = geography.provinces
         state_and_province_options = {'United States' => state_options['United States'].invert}.merge({'Canada' => province_options['Canada'].invert})
 
-        grouped_options_for_select(state_and_province_options)
+        grouped_options_for_select(state_and_province_options, selected)
       end
 
       def country_options_for_select
